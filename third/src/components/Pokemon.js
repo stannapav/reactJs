19b23d7e9b1e7682c4844a81839b7257dc70a113
Anna Striharchuk <stannapav@gmail.com>
2024-09-09 22:18:04 +0300

    lil fix

--- third/src/components/Pokemon.js
+++ third/src/components/Pokemon.js
@@ -1,15 +1,20 @@
 import React from "react";
 
 function Pokemon(pokemon) {
+  let weightText =
+    pokemon.weight <= 17 ? "light" : pokemon.weight >= 19 ? "heavy" : "";
+
   return (
     <div className="pokemon-card">
-      <img src={pokemon.image} />
+      <img src={pokemon.image} alt="" />
       <h3>{pokemon.name}</h3>
 
       <div className="info-group">
         <p>
           Height: {pokemon.height} <br />
-          Weight: {pokemon.weight} lbs <br />
+          Weight: {pokemon.weight} lbs
+          {weightText && <small> {weightText}</small>}
+          <br />
           Type: {pokemon.type}
         </p>
       </div>
