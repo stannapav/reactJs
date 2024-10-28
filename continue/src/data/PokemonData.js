0f8ade80e65d31c1b4e896a460af1cd58700dca5
Anna Striharchuk <stannapav@gmail.com>
2024-10-28 15:24:38 +0200

    toggle continue done

---
+++ continue/src/data/PokemonData.js
@@ -0,0 +1,52 @@
+const pokemons = [
+  {
+    id: 1,
+    name: "Bulbasaur",
+    height: "2' 04",
+    weight: 15.2,
+    type: "Grass, Poison",
+    starter: true,
+    isObtained: false,
+    imagePath:
+      "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png",
+    linkToPokedex: "https://www.pokemon.com/us/pokedex/bulbasaur",
+  },
+  {
+    id: 2,
+    name: "Pidgeot",
+    height: "4' 11",
+    weight: 87.1,
+    type: "Normal, Flying",
+    starter: false,
+    isObtained: false,
+    imagePath:
+      "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/018.png",
+    linkToPokedex: "https://www.pokemon.com/us/pokedex/pidgeot",
+  },
+  {
+    id: 3,
+    name: "Squirtle",
+    height: "1' 08",
+    weight: 19.8,
+    type: "Water",
+    starter: true,
+    isObtained: true,
+    imagePath:
+      "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/007.png",
+    linkToPokedex: "https://www.pokemon.com/us/pokedex/squirtle",
+  },
+  {
+    id: 4,
+    name: "Ditto",
+    height: "1' 00",
+    weight: 8.8,
+    type: "Normal",
+    starter: false,
+    isObtained: false,
+    imagePath:
+      "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/132.png",
+    linkToPokedex: "https://www.pokemon.com/us/pokedex/ditto",
+  },
+];
+
+export default pokemons;
