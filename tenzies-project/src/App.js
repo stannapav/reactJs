a7c396d23c1e0d3e067d1a87be7c55f544cb5916
stannapav <stannapav@gmail.com>
2025-02-19 23:40:25 +0200

    tenzies project base done

--- tenzies-project/src/App.js
+++ tenzies-project/src/App.js
@@ -1,20 +1,36 @@
+import { useState } from "react";
 import Die from "./components/Die";
+import { nanoid } from "nanoid";
 
 export default function App() {
+  const [dice, setDice] = useState(allNewDice());
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid(),
+      });
+    }
+    return newDice;
+  }
+
+  function rollDice() {
+    setDice(allNewDice());
+  }
+
+  const diceElements = dice.map((die) => (
+    <Die key={die.id} value={die.value} />
+  ));
+
   return (
     <main>
-      <div className="dice-container">
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-      </div>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        Roll
+      </button>
     </main>
   );
 }
