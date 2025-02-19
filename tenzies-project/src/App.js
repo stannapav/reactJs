cac0f0d809b08f1787a8968d709f0c1f7c4f968f
stannapav <stannapav@gmail.com>
2025-02-19 23:30:02 +0200

    Tenzies Project intro done

---
+++ tenzies-project/src/App.js
@@ -0,0 +1,20 @@
+import Die from "./components/Die";
+
+export default function App() {
+  return (
+    <main>
+      <div className="dice-container">
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+      </div>
+    </main>
+  );
+}
